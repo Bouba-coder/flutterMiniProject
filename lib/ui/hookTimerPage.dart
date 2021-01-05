0b509c881191c15a3d0932cacc36728859417242
yatera <boubacar.yatera@edu.itescia.fr>
2021-01-05 19:11:12 +0100

    Hook Package adding

--- lib/ui/hookTimerPage.dart
+++ lib/ui/hookTimerPage.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_hooks/flutter_hooks.dart';
+/*
 class HTimerPage extends StatefulWidget {
   @override
   _HTimerPageState createState() => _HTimerPageState();
 }
 
 class _HTimerPageState extends State<HTimerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Timer Hook Page"),
+        centerTitle: true,
+      ),
+      body: new Center(
+        child: new Text("This is timer hooks page."),
+      ),
+    );
+  }
+}
+*/
+class HTimerPage extends HookWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
